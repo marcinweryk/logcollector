cc1db97989a3ab38ffa658717474b5b5aebe3ba0
Marcin Weryk <marcin.weryk@verint.com>
2018-04-18 13:51:31 +0100

    search button added

--- frontend.py
+++ frontend.py
@@ -8,18 +8,22 @@ listbox.pack()
 
 listbox.insert(END, "List of log files:")
 
-#find all the log type files in the directory
-result = collector.find('*.log', 'C:\mytestdirectory')
 
-#open file to save the results of search
-f = open('logfileslist.txt','w')
+def searchforme():
+    # find all the log type files in the directory
+    result = collector.find('*.log', 'C:\mytestdirectory')
 
-# all results per line are saved in the file
-for files in result:
-    filelinef = ("File: {} | size: {} MB | Last modified: {} \n".format(files,("%.2f" % collector.convertbyte(collector.getsizeoffile(files))), collector.getlastmoddate(files) ))
-    f.write(str(filelinef))
-    listbox.insert(END, filelinef)
-#close the file
-f.close()
+    # open file to save the results of search
+    f = open('logfileslist.txt', 'w')
+
+    # all results per line are saved in the file
+    for files in result:
+        filelinef = ("File: {} | size: {} MB | Last modified: {} \n".format(files,("%.2f" % collector.convertbyte(collector.getsizeoffile(files))), collector.getlastmoddate(files) ))
+        f.write(str(filelinef))
+        listbox.insert(END, filelinef)
+    f.close()
+
+button1 = Button(text="Search", command = searchforme)
+button1.pack()
 
 mainloop()
