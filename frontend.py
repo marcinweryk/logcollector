92479cb4df9cba334823e12824479e11a61ed669
Marcin Weryk <marcin.weryk@verint.com>
2018-04-26 16:09:00 +0100

    Error handling and error logging basic functionality added

--- frontend.py
+++ frontend.py
@@ -1,6 +1,14 @@
 import collector
 from tkinter import *
 from tkinter import filedialog
+import traceback
+import logging
+
+###################log info #########################
+logging.basicConfig(filename='collector.log', level=logging.DEBUG, format='%(message)s %(asctime)s')
+logging.info('Started')
+############### log start ######################
+
 
 master = Tk()
 master.resizable(FALSE,FALSE)
@@ -17,32 +25,36 @@ scrollbar.config(command=listbox.yview)
 
 e = Entry(master)
 e.insert(END, "*.log")
-e.pack()
+e.pack(side=LEFT)
 e1 = Entry(master)
 e1.insert(END, "c:\FolderName")
-e1.pack()
+e1.pack(side=LEFT)
 
 def searchforme():
     # find all the log type files in the directory
-    #default values '*.log', 'c:\foldername'
-    file = e.get()
-    path = e1.get()
-    result = collector.find(file, path)
-    listbox.delete(0,END)
-    # all results per line are shown on the screen
-    for files in result:
-        filelinef = ("File: {} | size: {} MB | Last modified: {} \n".format(files,("%.2f" % collector.convertbyte(collector.getsizeoffile(files))), collector.getlastmoddate(files) ))
-        listbox.insert(END, filelinef)
+    try:
+        result = collector.find(e.get(), e1.get())
+        listbox.delete(0,END)
+        sizeOfWin = []
+        # all results per line are shown on the screen
+        for files in result:
+            filelinef = ("File: {} | size: {} MB | Last modified: {} \n".format(files,("%.2f" % collector.convertbyte(collector.getsizeoffile(files))), collector.getlastmoddate(files) ))
+            listbox.insert(END, filelinef)
+            sizeOfWin.append(len(filelinef))
+        listbox.config(width=max(sizeOfWin))
+    except:
+        listbox.insert(END, "No results - Check provided file name and path !")
+        logging.exception("Error in the application")
 
 def file_save():
     """get a filename and save the text in the editor widget"""
     # default extension is optional, here will add .txt if missing
-    #result = collector.find('*.log', 'C:\mytestdirectory')
     print(listbox.get(ACTIVE))
     result = listbox.get(ACTIVE)
     print(result)
     mask = \
         [("Text files", "*.txt"),
+         ("CSV","*.csv"),
          ("All files", "*.*")]
     fout = filedialog.asksaveasfile(mode='w', defaultextension=mask ,filetypes=mask)
     for files in result:
@@ -53,18 +65,18 @@ def file_save():
 def do_exit():
     master.destroy()
 
+button1 = Button(text="Search", command=searchforme)
+button1.pack(side=LEFT)
 menu = Menu(master)
 master.config(menu=menu)
 # file menu
 filemenu = Menu(menu, tearoff=0)
 menu.add_cascade(label="Options", menu=filemenu)
-filemenu.add_command(label="Search", command=searchforme)
-filemenu.add_separator()
 filemenu.add_command(label="Save", command=file_save)
 filemenu.add_separator()
 filemenu.add_command(label="Exit", command=do_exit)
-
-#button1 = Button(text="Search", command=searchforme)
-#button1.pack(side=LEFT)
 master.protocol("WM_DELETE_WINDOW", master.quit())
 mainloop()
+###############log ######################
+logging.info('Finished')
+###############log ######################
